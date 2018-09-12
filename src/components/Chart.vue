7ef17e33e11fbe9c949d7bd6f05dc69da2a0b829
nn605g <nn605g@intl.att.com>
2018-09-12 22:31:35 +0300

    🎉🎉🎉

---
+++ src/components/Chart.vue
@@ -0,0 +1,137 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  mounted: function() {
+    const width = 960;
+    const height = 600;
+
+    const svg = d3
+      .select(this.$el)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const simulation = d3
+      .forceSimulation()
+      .force(
+        "link",
+        d3.forceLink().id(function(d) {
+          return d.id;
+        })
+      )
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2));
+
+    fetch("miserables.json")
+      .then(r => r.json())
+      .then(graph => {
+        const link = svg
+          .append("g")
+          .attr("class", "links")
+          .selectAll("line")
+          .data(graph.links)
+          .enter()
+          .append("line")
+          .attr("stroke-width", function(d) {
+            return Math.sqrt(d.value);
+          });
+
+        const node = svg
+          .append("g")
+          .attr("class", "nodes")
+          .selectAll("circle")
+          .data(graph.nodes)
+          .enter()
+          .append("circle")
+          .attr("r", 5)
+          .attr("fill", function(d) {
+            return color(d.group);
+          })
+          .call(
+            d3
+              .drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended)
+          );
+
+        node.append("title").text(function(d) {
+          return d.id;
+        });
+
+        simulation.nodes(graph.nodes).on("tick", ticked);
+
+        simulation.force("link").links(graph.links);
+
+        function ticked() {
+          link
+            .attr("x1", function(d) {
+              return d.source.x;
+            })
+            .attr("y1", function(d) {
+              return d.source.y;
+            })
+            .attr("x2", function(d) {
+              return d.target.x;
+            })
+            .attr("y2", function(d) {
+              return d.target.y;
+            });
+
+          node
+            .attr("cx", function(d) {
+              return d.x;
+            })
+            .attr("cy", function(d) {
+              return d.y;
+            });
+        }
+      });
+
+    function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+svg {
+  margin: 25px;
+  path {
+    fill: none;
+    stroke: #76bf8a;
+    stroke-width: 3px;
+  }
+}
+
+.links line {
+  stroke: #999;
+  stroke-opacity: 0.6;
+}
+
+.nodes circle {
+  stroke: #fff;
+  stroke-width: 1.5px;
+}
+</style>
